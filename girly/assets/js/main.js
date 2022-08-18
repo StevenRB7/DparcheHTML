56fb756af3c76ccac582cb139774cf6b52bf3750
StevenRB7 <estarosero@misena.edu.co>
2022-08-18 11:06:41 -0500

    hp enano

--- girly/assets/js/main.js
+++ girly/assets/js/main.js
@@ -48,7 +48,31 @@ jQuery(document).ready(function(){
     }
   }
   );
+	var goToTop = function() {
 
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
   jQuery(".owl-carousel4").owlCarousel({
     loop:true,
     center: false,
